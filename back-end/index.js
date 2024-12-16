87f4d0d6fd8c2629c3dfa051949ab8ffc03e0be9
Yahya Zakaria <yz3004629@gmail.com>
2024-12-16 13:38:58 +0200

    handle code

--- back-end/index.js
+++ back-end/index.js
@@ -4,21 +4,37 @@ import authRoute from "./src/routes/auth.route.js";
 import messageRoute from "./src/routes/message.route.js";
 import { connectDB } from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 import cors from "cors";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://vercel.live"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:"],
+        connectSrc: ["'self'", "wss://vercel.live"],
+      },
+    },
+  })
+);
 
 // global middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-  origin: "https://bluechat-api.vercel.app" || "http://localhost:3001",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://bluechat-api.vercel.app" || "http://localhost:3001",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
